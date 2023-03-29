739511b1d4c296a7ad82322f3364988ac9d43159
DESKTOP-0TCVJJM\LENOVO <ltthang239@gmail.com>
2023-03-29 14:30:36 +0700

    init project

---
+++ HGWork.Model/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HGWork.Model
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool IsAdmin { get; set; }
+        public int Status { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public virtual ICollection<Project>? Projects { get; set; }
+    }
+}
---
+++ HGWork/Controllers/ProjectController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HGWork.Controllers
+{
+    [ApiController]
+    [Route("project")]
+    public class ProjectController : ControllerBase
+    {
+        [HttpGet]
+        public int Index()
+        {
+            return 0;
+        }
+    }
+}
